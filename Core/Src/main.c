11eec2d464286eab4eebc6724527e22a148109ba
jmac1 <145376972+JMacPort@users.noreply.github.com>
2024-11-26 13:29:05 -0500

    Need to fix getchar() to properly pull and register from SR and DR

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,5 +1,52 @@
 #include "main.h"
+#include <stdio.h>
+
+// Prototypes
+void UART_Init();
 
 int main() {
+    UART_Init();
 
+    while(1) {
+    	while(!(USART2 -> SR & (1 << 5)));
+        char c = USART2 -> DR;              		// Read data
+
+        printf("Got: %c\r\n", c);         		// Echo it back
+        USART2 -> SR &= ~(1 << 6);
+    }
 }
+
+void UART_Init() {
+	RCC -> APB1ENR |= (1 << 17);				// Enable UART2 Clock
+	RCC -> AHB1ENR |= (1 << 0);					// Enable GPIOA Clock
+
+	GPIOA -> MODER &= ~(3 << (2 * 2));			// Set PA2 to Alternate Function
+	GPIOA -> MODER |= (2 << (2 * 2));
+	GPIOA -> MODER &= ~(3 << (2 * 3));			// Set PA3 to Alternate Function
+	GPIOA -> MODER |= (2 << (2 * 3));
+
+	GPIOA -> AFR[0] |= (7 << 8);				// Set function to UART2
+	GPIOA -> AFR[0] |= (7 << 12);
+
+	USART2 -> BRR = 0x0683;						// Set baud rate to 9600
+
+	USART2 -> CR1 |= (1 << 3);					// Transmit Enabled
+	USART2 -> CR1 |= (1 << 2);					// Receive Enabled
+	USART2 -> CR1 |= (1 << 13);					// UART2 Enabled
+}
+
+int __io_putchar(int c) {
+    while(!(USART2 -> SR & (1 << 7)));
+    USART2 -> DR = c;
+
+    while(!(USART2 -> SR & (1 << 6)));
+    USART2 -> SR &= ~(1 << 6);
+    USART2 -> SR &= ~(1 << 5);
+
+    return c;
+}
+
+//int __io_getchar() {
+//	while(!(USART2 -> SR & (1 << 5)));
+//	return USART2 -> DR;
+//}
--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # UART Basics
+Simple UART communication from the STM32-F466RE to a PuTTY serial terminal. The output is simply echo'd back to the user for a basic demonstration.
 
 # Hardware
+- STM32-F466RE
 
 # Features
+- UART Communication
+- Character echoing
 
 # Pins 
+- PA2
+- PA3
